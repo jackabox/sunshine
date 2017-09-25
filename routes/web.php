1a94fec4756d55932cdc9b1f411ce6e3e50749c2
Jack Whiting <jackwhiting@macbook-pro.home>
2017-09-25 22:33:15 +0100

    Auth

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,7 @@ Route::get('documentation/{id}', 'DocumentationController@docs');
 
 Route::prefix('documentation')->group(function () {
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
